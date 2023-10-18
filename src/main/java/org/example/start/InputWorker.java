9338701ea39da5eb777dde59af5fa8838f381794
filuimeyo <nikakud24@gmail.com>
2023-10-18 14:25:35 +0300

    added input check

--- src/main/java/org/example/start/InputWorker.java
+++ src/main/java/org/example/start/InputWorker.java
@@ -1,6 +1,11 @@
 package org.example.start;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
 import java.sql.Date;
+import java.text.DecimalFormat;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
@@ -17,14 +22,14 @@ public class InputWorker {
                 if (mobileNum.matches("(^(\\+375|80)(29|33|44|25|17)\\d{7}$)")) return mobileNum;
                 else {
                     System.out.print("enter again, u entered: ");
-                    System.out.println(mobileNum.isEmpty()? "nothing": mobileNum);
+                    System.out.println(mobileNum.isEmpty() ? "nothing" : mobileNum);
                 }
             }
         }
 
     }
 
-    public static String enterStringWithRegex(String text, String excText, String regex){
+    public static String enterStringWithRegex(String text, String excText, String regex) {
         String str = null;
 
         Scanner scanner = new Scanner(System.in);
@@ -42,28 +47,43 @@ public class InputWorker {
 
     }
 
-    public static Long enterLong(){
-        Scanner scanner = new Scanner(System.in);
-        Long num;
+    public static int enterInt(String text, String errorText) {
+
+        BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+
+        String numString = null;
+        System.out.println(text);
         while (true) {
-           /* try {
-                System.out.println("enter long: ");
-                num = scanner.nextLong();
-                return num;
-            } catch (java.util.InputMismatchException e) {
-                System.out.println("enter again: ");
+            try {
+                numString = stdin.readLine();
+                return Integer.parseInt(numString);
 
-            }*/
-
-            if (scanner.hasNextLong()) {
-                num = scanner.nextLong();
-               return num;
-            } else {
-                System.out.println("Извините, но это явно не число. Перезапустите программу и попробуйте снова!");
+            } catch (NumberFormatException e) {
+                System.out.println(errorText + numString);
+            } catch (IOException e) {
+                System.out.println("U entered nothing");
             }
         }
     }
 
+    public static BigDecimal enterBigDecimal(String text, String errorText) {
+
+        BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+
+        String numString = null;
+        System.out.println(text);
+        while (true) {
+            try {
+                numString = stdin.readLine();
+                return new BigDecimal(numString);
+
+            } catch (NumberFormatException e) {
+                System.out.println(errorText + numString);
+            } catch (IOException e) {
+                System.out.println("U entered nothing");
+            }
+        }
+    }
 
   /*  public static String enterDate() {
         String date = null;
@@ -87,32 +107,32 @@ public class InputWorker {
     }
 */
 
-  /*  public static Date enterDate(){
-        String dateStr;
-        try (Scanner scanner = new Scanner(System.in)) {
-            while (true) {
+    /*  public static Date enterDate(){
+          String dateStr;
+          try (Scanner scanner = new Scanner(System.in)) {
+              while (true) {
 
-                System.out.println("Enter date:");
-                dateStr = scanner.next();
+                  System.out.println("Enter date:");
+                  dateStr = scanner.next();
 
 
-                if (dateStr.matches("((19[6-9]\\d)|(^20\\d{2}))-(0[1-9]|(1[0-2]))-((0[1-9]$)|([1-2][0-9]$)|(3[0-1]$))")) {
-                    return Date.valueOf(dateStr);
-                }
-                else {
-                    System.out.print("enter again, u entered: ");
-                    System.out.println(dateStr.isEmpty()? "nothing": dateStr);
-                }
-            }
-        } catch (NoSuchElementException e){
-            e.printStackTrace();
-        }
+                  if (dateStr.matches("((19[6-9]\\d)|(^20\\d{2}))-(0[1-9]|(1[0-2]))-((0[1-9]$)|([1-2][0-9]$)|(3[0-1]$))")) {
+                      return Date.valueOf(dateStr);
+                  }
+                  else {
+                      System.out.print("enter again, u entered: ");
+                      System.out.println(dateStr.isEmpty()? "nothing": dateStr);
+                  }
+              }
+          } catch (NoSuchElementException e){
+              e.printStackTrace();
+          }
 
-    }
-*/
+      }
+  */
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        /*Scanner sc = new Scanner(System.in);
         Long v =  sc.nextLong();
 
         String m = enterStringWithRegex("Enter date:", "Enter again, u entered: ", "((19[6-9]\\d)|(^20\\d{2}))-(0[1-9]|(1[0-2]))-((0[1-9]$)|([1-2][0-9]$)|(3[0-1]$))");
@@ -125,8 +145,19 @@ public class InputWorker {
 
         m = enterStringWithRegex("Enter email:", "Enter again, u entered: ", "^[\\w\\-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$");
         System.out.println(m);
+*/
+
+        int r = enterInt("Enter id", "Please enter int value, u entered: ");
+        System.out.println(r);
+
+        String m = enterStringWithRegex("Enter first name:", "Enter again, u entered: ", "^([a-zA-Z]{2,}-?[a-zA-Z]{2,})");
+        System.out.println(m);
+
+        m = enterStringWithRegex("Enter last name:", "Enter again, u entered: ", "^([a-zA-Z]{1,}\\'?-?[a-zA-Z]{2,}\\s?([a-zA-Z]{1,})?)");
+        System.out.println(m);
 
 
+        BigDecimal d = enterBigDecimal("Enter salary", "Please enter decimal value, u entered: ");
 
     }
 }
